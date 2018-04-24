8ab00e71a5f850d1a2869f386850798de8927c5c
rodrigodavid <rodrigo.monney@gmail.com>
2018-04-24 05:16:23 -0300

    Created orders page; updated some page style; created routes.

--- order-app-front/src/app/app.module.ts
+++ order-app-front/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
-
+import { OrderComponent } from './order/order.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '', component: HomeComponent
+      },
+      {
+        path: 'orders', component: OrderComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
