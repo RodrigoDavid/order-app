ce0baf3602f2f27e59d101c3b603b4760b1307f2
rodrigodavid <rodrigo.monney@gmail.com>
2018-04-25 02:53:17 -0300

    Changes to back end: CORS middleware added.

--- order-app-back/app.js
+++ order-app-back/app.js
@@ -3,21 +3,29 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+// const cors = require('cors');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const ordersRouter = require('./routes/orders.route');
 
 const app = express();
 
+//CORS middleware
+const allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+};
+
+app.use(allowCrossDomain);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/orders', ordersRouter);
 
 // catch 404 and forward to error handler
